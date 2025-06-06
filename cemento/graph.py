0db06d8aa7595a8ce043b13f0a99ab3dafd9db06
Gabbyton <gponon2000@gmail.com>
2025-06-06 11:58:27 -0400

    added id generation for when inputting graphs

--- cemento/graph.py
+++ cemento/graph.py
@@ -49,10 +49,25 @@ class Graph:
 
         if graph:
             self._edges = list(graph.edges())
+            self._nodes = self._graph.nodes()
+
+            if do_gen_ids:
+                terms = {term_id: term for term_id, term in enumerate(self._graph.nodes)}
+                inv_terms = {term: term_id for term_id, term in terms.items()}
+                rels_id = {edge: rel_id for rel_id, edge in enumerate(self._graph.edges)}
+                rels = {edge_id: self._graph.get_edge_data(parent, child)['label'] for (parent, child), edge_id in rels_id.items()}
+                # assume rank for drawing
+                # TODO: change to more sophisticated screening once graph conmplete
+                edges = [(inv_terms[parent], inv_terms[child], "rank") for (parent, child) in rels_id.keys()]
+                
+                ref = GraphRef(terms, rels, rels_id)
+
+                self._graph = nx.relabel_nodes(self._graph, inv_terms)
+                self._edges = edges
+                self._nodes = self._graph.nodes()
+
             if ref:
                 self._ref = ref
-
-        self._nodes = self._graph.nodes()
 
     def _read_edges(self, do_gen_ids, infer_rank, file_path=None, rels_df=None):
 
