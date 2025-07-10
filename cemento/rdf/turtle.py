d49aee4e71224f9a87f1cdb99da55d1f3b90eed5
Gabbyton <gponon2000@gmail.com>
2025-07-10 12:48:28 -0400

    removed all set except graph

--- cemento/rdf/turtle.py
+++ cemento/rdf/turtle.py
@@ -1,15 +1,23 @@
+from collections import defaultdict
+
+import networkx as nx
 import rdflib
 from rdflib.namespace import split_uri
+
 from cemento.graph import Graph
-import networkx as nx
-from collections import defaultdict
-import pandas as pd
-import numpy as np
+
 
 class Turtle(Graph):
 
-    def __init__(self, file_path):
-        super().__init__(file_path, rels_df=None, graph=None, ref=None, do_gen_ids=True, infer_rank=False)
+    def __init__(self, file_path, include_labels=True):
+        super().__init__(
+            file_path,
+            rels_df=None,
+            graph=None,
+            ref=None,
+            do_gen_ids=True,
+            infer_rank=False,
+        )
         self._file_path = file_path
         self._rdf_graph = None
         self._rename_vars = defaultdict(list)
@@ -17,10 +25,12 @@ class Turtle(Graph):
 
         # parse ttl file into graph
         self._rdf_graph = rdflib.Graph()
-        self._rdf_graph.parse(self._file_path, format='turtle')
+        self._rdf_graph.parse(self._file_path, format="turtle")
 
         # read through namespaces and store prefixes
-        self._prefixes = {str(value): key for key, value in self._rdf_graph.namespaces()}
+        self._prefixes = {
+            str(value): key for key, value in self._rdf_graph.namespaces()
+        }
 
         # get all accepted terms, object properties and classes
 
@@ -29,27 +39,39 @@ class Turtle(Graph):
         object_prop_triples = defaultdict(list)
         term_type = defaultdict(list)
         for subj, pred, obj in self._rdf_graph:
-            if pred == rdflib.RDFS.subClassOf and isinstance(subj, rdflib.URIRef) and isinstance(obj, rdflib.URIRef):
-                subclass_triples.append((subj, obj))
+            if (
+                pred == rdflib.RDFS.subClassOf
+                and isinstance(subj, rdflib.URIRef)
+                and isinstance(obj, rdflib.URIRef)
+            ):
+                subclass_triples.append((obj, subj))
                 class_terms.update([subj, obj])
-            
+
             if pred == rdflib.RDF.type:
                 term_type[obj].append(subj)
 
             if pred == rdflib.RDFS.label:
                 self._rename_vars[subj].insert(0, obj)
-            
+
             if pred == rdflib.SKOS.altLabel:
-                self._rename_vars[subj].append(obj) 
-        
+                self._rename_vars[subj].append(obj)
+
         for obj_prop in term_type[rdflib.OWL.ObjectProperty]:
-            for prop_subj, prop_pred, prop_obj in self._rdf_graph.triples((None, obj_prop, None)):
-                if isinstance(prop_subj, rdflib.URIRef) and isinstance(prop_obj, rdflib.URIRef):
+            for prop_subj, prop_pred, prop_obj in self._rdf_graph.triples(
+                (None, obj_prop, None)
+            ):
+                if isinstance(prop_subj, rdflib.URIRef) and isinstance(
+                    prop_obj, rdflib.URIRef
+                ):
                     object_prop_triples[prop_pred].append((subj, obj))
-        
-        all_classes = {term for term in self._rdf_graph.subjects(rdflib.RDF.type, rdflib.OWL.Class) if isinstance(term, rdflib.URIRef)}
+
+        all_classes = {
+            term
+            for term in self._rdf_graph.subjects(rdflib.RDF.type, rdflib.OWL.Class)
+            if isinstance(term, rdflib.URIRef)
+        }
         class_terms.update(all_classes - class_terms)
-        
+
         self._graph = nx.DiGraph()
         self._graph.add_edges_from(subclass_triples, label="rdfs:subClassOf")
         for pred, triples in object_prop_triples.items():
@@ -60,20 +82,17 @@ class Turtle(Graph):
         for term in all_classes:
             all_names = self.get_all_names(term)
             extra_name = f" ({','.join(all_names)})"
-            new_term_names[term] = f"{self.get_abbrev_name(term)}{extra_name if all_names else ''}"
-        # print(new_term_names)
+            new_term_names[term] = (
+                f"{self.get_abbrev_name(term)}{extra_name if include_labels and all_names else ''}"
+            )
         self._graph = nx.relabel_nodes(self._graph, new_term_names)
 
-        # get edge data
-        df_triples = [(parent, child, data['label']) for parent, child, data in self._graph.edges(data=True)]
-        self._rels_df = pd.DataFrame(np.array(df_triples), columns=['parent', 'child', 'rel'])
-       
     def get_rdf_graph(self):
         return self._rdf_graph
 
     def get_prefix(self, uri):
         return self._prefixes[uri]
-    
+
     def _get_rename_vars(self):
         return self._rename_vars
 
@@ -89,6 +108,6 @@ class Turtle(Graph):
             return rename_vars[term][0]
         except (IndexError, KeyError):
             return term
-    
+
     def get_all_names(self, term):
         return self._rename_vars[term]
