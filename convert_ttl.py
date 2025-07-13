07bd0bd1e23ae610db3bc8c2d97fdf5722548bab
Gabbyton <gponon2000@gmail.com>
2025-07-13 15:30:58 -0400

    added name extraction and labelling, subclassOf relationships reversed

--- convert_ttl.py
+++ convert_ttl.py
@@ -12,14 +12,14 @@ import networkx as nx
 import rdflib
 from networkx import DiGraph
 from pandas import DataFrame
-from rdflib import DCTERMS, OWL, RDF, RDFS, SKOS, Graph, Namespace, URIRef
+from rdflib import DCTERMS, OWL, RDF, RDFS, SKOS, Graph, Namespace, URIRef, Literal
 from rdflib.collection import Collection
 from rdflib.namespace import split_uri
 from thefuzz import fuzz, process
 
 from cemento.draw_io.read_diagram import ReadDiagram
 
-INPUT_PATH = "/Users/gabriel/dev/sdle/CEMENTO/sandbox/SyncXrayResult_graph.drawio"
+INPUT_PATH = "/Users/gabriel/dev/sdle/CEMENTO/sandbox/mdsChemRxn(v.0.03).drawio"
 ONTO_FOLDER = "/Users/gabriel/dev/sdle/CEMENTO/data"
 PREFIXES_PATH = "/Users/gabriel/dev/sdle/CEMENTO/sandbox/prefixes.json"
 TTL_OUTPUT_PATH = "/Users/gabriel/dev/sdle/CEMENTO/sandbox/output.ttl"
@@ -97,6 +97,20 @@ def get_search_terms_from_graph(
     return search_terms
 
 
+def remove_term_names(term: str) -> str:
+    match = re.search(r"^([^(]*)", term)
+    return match.group(1).strip() if match else term
+
+
+def get_term_aliases(term: str) -> list[str]:
+    match = re.search(r"\(([^)]*)\)", term)
+    if match:
+        alt_term_string = match.group(1)
+        alt_term_string = alt_term_string.split(",")
+        return [term.strip() for term in alt_term_string]
+    return []
+
+
 def get_abbrev_term(
     term: str, is_predicate=False, default_prefix="mds"
 ) -> tuple[str, str]:
@@ -104,6 +118,7 @@ def get_abbrev_term(
     abbrev_term = term
     strict_camel_case = False
 
+    term = remove_term_names(term)
     if ":" in term:
         prefix, abbrev_term = term.split(":")
 
@@ -205,7 +220,11 @@ def get_term_search_keys(term: str, inv_prefix: dict[URIRef, str]) -> list[str]:
     undo_camel_case_term = " ".join(
         re.findall(r"[A-Z]+(?=[A-Z][a-z]|\b)|[A-Z][a-z]+|[0-9]+", abbrev_term)
     )
-    search_keys = [term, f"{prefix}:{abbrev_term}", f"{prefix}:{undo_camel_case_term}"]
+    search_keys = [
+        remove_term_names(term),
+        f"{prefix}:{abbrev_term}",
+        f"{prefix}:{undo_camel_case_term}",
+    ]
     return [key.strip() for key in search_keys]
 
 
@@ -226,18 +245,26 @@ def term_in_search_results(
     term: URIRef,
     inv_prefixes: dict[URIRef | Namespace, str],
     search_terms: dict[str, URIRef],
+    invert=False,
 ) -> URIRef:
-    return get_term_search_result(term, inv_prefixes, search_terms) is not None
+    eval_value = get_term_search_result(term, inv_prefixes, search_terms) is not None
+    if invert:
+        eval_value = not eval_value
+    return eval_value
 
 
 def term_not_in_default_namespace(
     term: URIRef,
     inv_prefixes: dict[URIRef | Namespace, str],
     default_namespace_prefixes: dict[str, Namespace],
+    invert=False,
 ) -> bool:
     ns, abbrev_term = split_uri(term)
     prefix = inv_prefixes[str(ns)]
-    return prefix not in default_namespace_prefixes
+    eval_value = prefix not in default_namespace_prefixes
+    if invert:
+        eval_value = not eval_value
+    return eval_value
 
 
 def iter_graph_terms(
@@ -307,6 +334,16 @@ def add_exact_matches(
     rdf_graph.add((term, SKOS.exactMatch, term))
 
 
+def add_labels(term: URIRef, labels: list[str], rdf_graph: Graph) -> Graph:
+    # assume the first element of the labels is the actual label, others are alt-names
+    if labels:
+        rdf_graph.add((term, RDFS.label, Literal(labels[0])))
+        if len(labels) > 1:
+            for label in labels[1:]:
+                rdf_graph.add((term, SKOS.altLabel, Literal(label)))
+    return rdf_graph
+
+
 if __name__ == "__main__":
     default_namespaces = [RDF, RDFS, OWL, DCTERMS, SKOS]
     default_namespace_prefixes = ["rdf", "rdfs", "owl", "dcterms", "skos"]
@@ -317,19 +354,27 @@ if __name__ == "__main__":
         prefix: ns for prefix, ns in zip(default_namespace_prefixes, default_namespaces)
     }
     prefixes.update(default_namespace_prefixes)
-
-    search_terms = get_search_terms_from_defaults(default_namespace_prefixes)
     file_prefixes = iterate_ttl_graphs(ONTO_FOLDER, read_prefixes_from_graph)
     prefixes |= merge_dictionaries(file_prefixes)
+
     inv_prefixes = {value: key for key, value in prefixes.items()}
+
+    search_terms = get_search_terms_from_defaults(default_namespace_prefixes)
     file_search_terms = iterate_ttl_graphs(
         ONTO_FOLDER, partial(get_search_terms_from_graph, inv_prefixes=inv_prefixes)
     )
     search_terms |= merge_dictionaries(file_search_terms)
 
     # read the diagram and retrieve the relationship triples as a dataframe
-    diagram = ReadDiagram(INPUT_PATH)
+    diagram = ReadDiagram(INPUT_PATH, inverted_rank_arrows=True)
     rels = diagram.get_relationships()
+
+    aliases = {
+        term: aliases
+        for term, aliases in iter_diagram_terms(
+            rels, lambda term: (term, get_term_aliases(term))
+        )
+    }
 
     constructed_terms = {
         term: term_uri_ref
@@ -358,6 +403,8 @@ if __name__ == "__main__":
         if substituted_value is not None
     }
 
+    inv_constructed_terms = {value: key for key, value in constructed_terms.items()}
+
     constructed_terms.update(substitution_results)
 
     graph = nx.DiGraph()
@@ -384,15 +431,15 @@ if __name__ == "__main__":
     # if the term is a predicate and is not part of the default namespaces, add an object property type to the ttl file
     for term in predicate_terms:
         # TODO: Assume all predicates are object properties for now, change later
-        if not term_not_in_default_namespace(
+        if term_not_in_default_namespace(
             term, inv_prefixes, default_namespace_prefixes
         ):
             rdf_graph.add((term, RDF.type, OWL.ObjectProperty))
 
-    props = [RDF.value, RDFS.label]
+    exact_match_properties = [RDF.value, RDFS.label]
     results = {
         term: {prop: value}
-        for prop in props
+        for prop in exact_match_properties
         for result in iterate_ttl_graphs(
             ONTO_FOLDER,
             lambda rdf_graph, prop=prop: iter_graph_terms(
@@ -406,22 +453,38 @@ if __name__ == "__main__":
         for term, value in result
     }
 
-    filter_functions = [
-        partial(
-            term_in_search_results, inv_prefixes=inv_prefixes, search_terms=search_terms
-        ),
-        partial(
-            term_not_in_default_namespace,
-            inv_prefixes=inv_prefixes,
-            default_namespace_prefixes=default_namespace_prefixes,
-        ),
-    ]
+    term_in_search_results_filter = partial(
+        term_in_search_results, inv_prefixes=inv_prefixes, search_terms=search_terms
+    )
+    term_not_in_default_namespace_filter = partial(
+        term_not_in_default_namespace,
+        inv_prefixes=inv_prefixes,
+        default_namespace_prefixes=default_namespace_prefixes,
+    )
+
     iter_graph_terms(
         all_terms,
         lambda graph_term: add_exact_matches(
             graph_term, match_properties=results[graph_term], rdf_graph=rdf_graph
         ),
-        filter_functions,
+        [term_in_search_results_filter, term_not_in_default_namespace_filter],
+    )
+    iter_graph_terms(
+        all_terms,
+        lambda graph_term: add_labels(
+            term=graph_term,
+            labels=aliases[inv_constructed_terms[graph_term]],
+            rdf_graph=rdf_graph,
+        ),
+        [
+            term_not_in_default_namespace_filter,
+            partial(
+                term_in_search_results,
+                inv_prefixes=inv_prefixes,
+                search_terms=search_terms,
+                invert=True,
+            ),
+        ],
     )
 
     # now add the triples from the drawio diagram
