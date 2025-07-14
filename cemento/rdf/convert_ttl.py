52a3b1fd90c6bed4f430996ef4dee6bdd255ea30
Gabbyton <gponon2000@gmail.com>
2025-07-13 21:45:56 -0400

    migrated funcs to transforms and filters from convert

--- cemento/rdf/convert_ttl.py
+++ cemento/rdf/convert_ttl.py
@@ -1,17 +1,11 @@
-import re
-from collections import defaultdict
-from collections.abc import Callable, Iterable
 from functools import partial
 
 import networkx as nx
 import rdflib
-from networkx import DiGraph
-from rdflib import DCTERMS, OWL, RDF, RDFS, SKOS, Graph, Literal, Namespace, URIRef
-from rdflib.collection import Collection
-from rdflib.namespace import split_uri
-from thefuzz import fuzz, process
+from rdflib import DCTERMS, OWL, RDF, RDFS, SKOS
 
 from cemento.draw_io.read_diagram import ReadDiagram
+from cemento.rdf.filters import term_in_search_results, term_not_in_default_namespace
 from cemento.rdf.io import (
     get_search_terms_from_defaults,
     get_search_terms_from_graph,
@@ -24,7 +18,17 @@ from cemento.rdf.preprocessing import (
     get_abbrev_term,
     get_term_aliases,
     merge_dictionaries,
-    remove_term_names,
+)
+from cemento.rdf.transforms import (
+    add_exact_matches,
+    add_labels,
+    bind_prefixes,
+    construct_term_uri,
+    get_class_terms,
+    get_term_search_keys,
+    get_term_value,
+    iter_graph_terms,
+    substitute_term,
 )
 
 INPUT_PATH = "/Users/gabriel/dev/sdle/CEMENTO/sandbox/recipe.drawio"
@@ -32,189 +36,6 @@ ONTO_FOLDER = "/Users/gabriel/dev/sdle/CEMENTO/data"
 PREFIXES_PATH = "/Users/gabriel/dev/sdle/CEMENTO/sandbox/prefixes.json"
 TTL_OUTPUT_PATH = "/Users/gabriel/dev/sdle/CEMENTO/sandbox/recipe.ttl"
 DRAWIO_OUTPUT_PATH = "/Users/gabriel/dev/sdle/CEMENTO/sandbox/recipe-redraw.drawio"
-
-
-def construct_term_uri(
-    prefix: str,
-    abbrev_term: str,
-    prefixes: dict[str, URIRef | Namespace],
-) -> URIRef:
-    ns_uri = prefixes[prefix]
-    return rdflib.URIRef(f"{ns_uri}{abbrev_term}")
-
-
-def substitute_term(
-    search_keys: Iterable[str], search_terms: dict[str, URIRef]
-) -> URIRef:
-    best_match, score = max(
-        (
-            result
-            for search_key in search_keys
-            if (
-                result := process.extractOne(
-                    search_key,
-                    search_terms.keys(),
-                    scorer=fuzz.token_sort_ratio,
-                    score_cutoff=75,
-                )
-            )
-            is not None
-        ),
-        key=lambda x: x[1] if x is not None else -1,
-        default=(None, -1),
-    )
-    return search_terms[best_match] if best_match else None
-
-
-def bind_prefixes(rdf_graph: Graph, prefixes: dict[str, URIRef | Namespace]) -> Graph:
-    for prefix, ns in prefixes.items():
-        rdf_graph.bind(prefix, ns)
-    return rdf_graph
-
-
-def get_class_terms(graph: DiGraph) -> set[URIRef]:
-    class_terms = set()
-    for subj, obj, data in graph.edges(data=True):
-        predicate = data["label"]
-
-        if predicate == RDFS.subClassOf:
-            # we assume terms between RDFS.subClassOf is a class
-            class_terms.update([subj, obj])
-
-        if predicate == RDF.type:
-            # we assume the object of an RDF.type is also a class
-            class_terms.add(obj)
-
-    return class_terms
-
-
-def get_term_search_keys(term: str, inv_prefix: dict[URIRef, str]) -> list[str]:
-    prefix, abbrev_term = get_abbrev_term(term)
-    undo_camel_case_term = " ".join(
-        re.findall(r"[A-Z]+(?=[A-Z][a-z]|\b)|[A-Z][a-z]+|[0-9]+", abbrev_term)
-    )
-    search_keys = [
-        remove_term_names(term),
-        f"{prefix}:{abbrev_term}",
-        f"{prefix}:{undo_camel_case_term}",
-    ]
-    return [key.strip() for key in search_keys]
-
-
-def get_term_search_result(
-    term: URIRef,
-    inv_prefixes: dict[URIRef | Namespace, str],
-    search_terms: dict[str, URIRef],
-) -> URIRef:
-    ns, abbrev_term = split_uri(term)
-    prefix = inv_prefixes[str(ns)]
-    search_term = f"{prefix}:{abbrev_term}"
-    if search_term in search_terms:
-        return search_terms[search_term]
-    return None
-
-
-def term_in_search_results(
-    term: URIRef,
-    inv_prefixes: dict[URIRef | Namespace, str],
-    search_terms: dict[str, URIRef],
-    invert=False,
-) -> URIRef:
-    eval_value = get_term_search_result(term, inv_prefixes, search_terms) is not None
-    if invert:
-        eval_value = not eval_value
-    return eval_value
-
-
-def term_not_in_default_namespace(
-    term: URIRef,
-    inv_prefixes: dict[URIRef | Namespace, str],
-    default_namespace_prefixes: dict[str, Namespace],
-    invert=False,
-) -> bool:
-    ns, abbrev_term = split_uri(term)
-    prefix = inv_prefixes[str(ns)]
-    eval_value = prefix not in default_namespace_prefixes
-    if invert:
-        eval_value = not eval_value
-    return eval_value
-
-
-def iter_graph_terms(
-    graph_terms: set[URIRef],
-    term_function: Callable[[URIRef], None],
-    filter_functions: Iterable[Callable[..., bool]] = None,
-    invert_filter=False,
-) -> list[any]:
-    results = []
-    for term in graph_terms:
-        eval_value = True
-        if filter_functions:
-            eval_value = all(
-                [filter_function(term) for filter_function in filter_functions]
-            )
-            if invert_filter:
-                eval_value = not eval_value
-
-        if eval_value:
-            results.append(term_function(term))
-    return results
-
-
-def add_domains_ranges(term: URIRef):
-    predicate_domain = defaultdict(list)
-    predicate_range = defaultdict(list)
-    for domain_term, range_term, data in graph.edges(data=True):
-        predicate_term = data["label"]
-        predicate_domain[predicate_term].append(domain_term)
-        predicate_range[predicate_term].append(range_term)
-
-    for dom_or_range_term, term_dom_range in {
-        RDFS.domain: predicate_domain[term],
-        RDFS.range: predicate_range[term],
-    }.items():
-        if term_dom_range:
-            # if there are more than one term, save the domain or range as a collection
-            if len(term_dom_range) > 1:
-                collection_node = rdflib.BNode()
-                Collection(rdf_graph, collection_node, term_dom_range)
-                # create class that points to the collection
-                collection_class = rdflib.BNode()
-                rdf_graph.add((collection_class, RDF.type, OWL.Class))
-                # connect them all together
-                # TODO: assume union for now but fix later
-                rdf_graph.add((collection_class, OWL.unionOf, collection_node))
-                rdf_graph.add((term, dom_or_range_term, collection_class))
-            else:
-                # if there is only one term, use that term directly
-                rdf_graph.add((term, dom_or_range_term, term_dom_range[0]))
-
-
-def get_term_value(subj: URIRef, pred: URIRef, ref_rdf_graph: Graph):
-    return ref_rdf_graph.value(subj, pred)
-
-
-def add_exact_matches(
-    term: URIRef, match_properties: dict[URIRef, URIRef | None], rdf_graph: Graph
-) -> Graph:
-    # if the term is already imported from somewhere else
-    # get the type and label if available and add to the ttl file
-    for match_property, value in match_properties.items():
-        if value:
-            rdf_graph.add((term, match_property, value))
-
-    # add an exact match to the ttl file for easier cross-referencing
-    rdf_graph.add((term, SKOS.exactMatch, term))
-
-
-def add_labels(term: URIRef, labels: list[str], rdf_graph: Graph) -> Graph:
-    # assume the first element of the labels is the actual label, others are alt-names
-    if labels:
-        rdf_graph.add((term, RDFS.label, Literal(labels[0])))
-        if len(labels) > 1:
-            for label in labels[1:]:
-                rdf_graph.add((term, SKOS.altLabel, Literal(label)))
-    return rdf_graph
 
 
 if __name__ == "__main__":
---
+++ cemento/rdf/transforms.py
@@ -0,0 +1,166 @@
+import re
+from collections.abc import Callable, Iterable
+from collections import defaultdict
+from networkx import DiGraph
+from rdflib import RDF, RDFS, SKOS, OWL, Graph, Literal, Namespace, BNode, URIRef
+from rdflib.collection import Collection
+from rdflib.namespace import split_uri
+from thefuzz import fuzz, process
+
+from cemento.preprocessing import get_abbrev_term, remove_term_names
+
+def construct_term_uri(
+    prefix: str,
+    abbrev_term: str,
+    prefixes: dict[str, URIRef | Namespace],
+) -> URIRef:
+    ns_uri = prefixes[prefix]
+    return URIRef(f"{ns_uri}{abbrev_term}")
+
+
+def substitute_term(
+    search_keys: Iterable[str], search_terms: dict[str, URIRef]
+) -> URIRef:
+    best_match, score = max(
+        (
+            result
+            for search_key in search_keys
+            if (
+                result := process.extractOne(
+                    search_key,
+                    search_terms.keys(),
+                    scorer=fuzz.token_sort_ratio,
+                    score_cutoff=75,
+                )
+            )
+            is not None
+        ),
+        key=lambda x: x[1] if x is not None else -1,
+        default=(None, -1),
+    )
+    return search_terms[best_match] if best_match else None
+
+
+def bind_prefixes(rdf_graph: Graph, prefixes: dict[str, URIRef | Namespace]) -> Graph:
+    for prefix, ns in prefixes.items():
+        rdf_graph.bind(prefix, ns)
+    return rdf_graph
+
+
+def get_class_terms(graph: DiGraph) -> set[URIRef]:
+    class_terms = set()
+    for subj, obj, data in graph.edges(data=True):
+        predicate = data["label"]
+
+        if predicate == RDFS.subClassOf:
+            # we assume terms between RDFS.subClassOf is a class
+            class_terms.update([subj, obj])
+
+        if predicate == RDF.type:
+            # we assume the object of an RDF.type is also a class
+            class_terms.add(obj)
+
+    return class_terms
+
+
+def get_term_search_keys(term: str, inv_prefix: dict[URIRef, str]) -> list[str]:
+    prefix, abbrev_term = get_abbrev_term(term)
+    undo_camel_case_term = " ".join(
+        re.findall(r"[A-Z]+(?=[A-Z][a-z]|\b)|[A-Z][a-z]+|[0-9]+", abbrev_term)
+    )
+    search_keys = [
+        remove_term_names(term),
+        f"{prefix}:{abbrev_term}",
+        f"{prefix}:{undo_camel_case_term}",
+    ]
+    return [key.strip() for key in search_keys]
+
+
+def get_term_search_result(
+    term: URIRef,
+    inv_prefixes: dict[URIRef | Namespace, str],
+    search_terms: dict[str, URIRef],
+) -> URIRef:
+    ns, abbrev_term = split_uri(term)
+    prefix = inv_prefixes[str(ns)]
+    search_term = f"{prefix}:{abbrev_term}"
+    if search_term in search_terms:
+        return search_terms[search_term]
+    return None
+
+
+def iter_graph_terms(
+    graph_terms: set[URIRef],
+    term_function: Callable[[URIRef], None],
+    filter_functions: Iterable[Callable[..., bool]] = None,
+    invert_filter=False,
+) -> list[any]:
+    results = []
+    for term in graph_terms:
+        eval_value = True
+        if filter_functions:
+            eval_value = all(
+                [filter_function(term) for filter_function in filter_functions]
+            )
+            if invert_filter:
+                eval_value = not eval_value
+
+        if eval_value:
+            results.append(term_function(term))
+    return results
+
+
+def get_term_value(subj: URIRef, pred: URIRef, ref_rdf_graph: Graph):
+    return ref_rdf_graph.value(subj, pred)
+
+
+def add_exact_matches(
+    term: URIRef, match_properties: dict[URIRef, URIRef | None], rdf_graph: Graph
+) -> Graph:
+    # if the term is already imported from somewhere else
+    # get the type and label if available and add to the ttl file
+    for match_property, value in match_properties.items():
+        if value:
+            rdf_graph.add((term, match_property, value))
+
+    # add an exact match to the ttl file for easier cross-referencing
+    rdf_graph.add((term, SKOS.exactMatch, term))
+
+
+def add_labels(term: URIRef, labels: list[str], rdf_graph: Graph) -> Graph:
+    # assume the first element of the labels is the actual label, others are alt-names
+    if labels:
+        rdf_graph.add((term, RDFS.label, Literal(labels[0])))
+        if len(labels) > 1:
+            for label in labels[1:]:
+                rdf_graph.add((term, SKOS.altLabel, Literal(label)))
+    return rdf_graph
+
+
+# def add_domains_ranges(term: URIRef):
+#     predicate_domain = defaultdict(list)
+#     predicate_range = defaultdict(list)
+#     for domain_term, range_term, data in graph.edges(data=True):
+#         predicate_term = data["label"]
+#         predicate_domain[predicate_term].append(domain_term)
+#         predicate_range[predicate_term].append(range_term)
+
+#     for dom_or_range_term, term_dom_range in {
+#         RDFS.domain: predicate_domain[term],
+#         RDFS.range: predicate_range[term],
+#     }.items():
+#         if term_dom_range:
+#             # if there are more than one term, save the domain or range as a collection
+#             if len(term_dom_range) > 1:
+#                 collection_node = BNode()
+#                 Collection(rdf_graph, collection_node, term_dom_range)
+#                 # create class that points to the collection
+#                 collection_class = rdflib.BNode()
+#                 rdf_graph.add((collection_class, RDF.type, OWL.Class))
+#                 # connect them all together
+#                 # TODO: assume union for now but fix later
+#                 rdf_graph.add((collection_class, OWL.unionOf, collection_node))
+#                 rdf_graph.add((term, dom_or_range_term, collection_class))
+#             else:
+#                 # if there is only one term, use that term directly
+#                 rdf_graph.add((term, dom_or_range_term, term_dom_range[0]))
