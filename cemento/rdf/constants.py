506dcd35a3548a330e50a4745f8f8c337ff3f2f9
Gabriel Ponon <gop2@case.edu>
2025-07-18 21:49:44 -0400

    added cli banner, added download subcommand, moved some constatns from rdf

--- cemento/rdf/constants.py
+++ cemento/rdf/constants.py
@@ -1,8 +1,3 @@
 from rdflib import OWL
 
 PREDICATES = {OWL.ObjectProperty, OWL.AnnotationProperty, OWL.DatatypeProperty}
-
-DEFAULT_DOWNLOADS = {
-    # cco as the default download
-    "cco": "https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/refs/heads/develop/src/cco-merged/CommonCoreOntologiesMerged.ttl"
-}
