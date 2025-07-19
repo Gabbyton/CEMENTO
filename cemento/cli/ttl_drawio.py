581fa66eee31758c2816f9f28bc7dfb3d2770fe5
Gabriel Ponon <gop2@case.edu>
2025-07-19 14:13:53 -0400

    added arguments for checking ttl and settting unique literals for ttl to drawio

--- cemento/cli/ttl_drawio.py
+++ cemento/cli/ttl_drawio.py
@@ -14,16 +14,34 @@ def register(subparsers):
         "output", help="the path to the desired output .ttl file.", metavar="file_path"
     )
     parser.add_argument(
-        "-z",
-        "--horizontal",
+        "-hz",
+        "--horizontal-graph",
         help="set whether to make the tree horizontal or stay with the default vertical layout.",
         metavar="is_horizontal",
         type=bool,
         default=False,
+    )
+    parser.add_argument(
+        "-dct",
+        "--dont-check-turtle",
+        help="set whether to stop evaluating turtle file for potential errors prior to conversion. Computation will be marginally shorter.",
+        action="store_false",
+    )
+    parser.add_argument(
+        "-nul",
+        "--no-unique-literals",
+        help="set whether to to append a unique id to each encountered literal term. Affects labels, definitions and any other literal values.",
+        action="store_false",
     )
     parser.set_defaults(_handler=run)
 
 
 def run(args):
     print(f"converting {args.input} into a drawio diagram at {args.output}...")
-    convert_ttl_to_drawio(args.input, args.output, args.horizontal)
+    convert_ttl_to_drawio(
+        args.input,
+        args.output,
+        args.horizontal_graph,
+        check_ttl_validity=args.dont_check_turtle,
+        set_unique_literals=args.no_unique_literals,
+    )
--- cemento/rdf/turtle_to_drawio.py
+++ cemento/rdf/turtle_to_drawio.py
@@ -5,7 +5,15 @@ from cemento.rdf.turtle_to_graph import convert_ttl_to_graph
 
 
 def convert_ttl_to_drawio(
-    input_path: str | Path, output_path: str | Path, horizontal_tree: bool = False
+    input_path: str | Path,
+    output_path: str | Path,
+    horizontal_tree: bool = False,
+    check_ttl_validity: bool = True,
+    set_unique_literals: bool = True,
 ):
-    graph = convert_ttl_to_graph(input_path)
+    graph = convert_ttl_to_graph(
+        input_path,
+        check_ttl_validity=check_ttl_validity,
+        set_unique_literals=set_unique_literals,
+    )
     draw_tree(graph, output_path, horizontal_tree=horizontal_tree)
