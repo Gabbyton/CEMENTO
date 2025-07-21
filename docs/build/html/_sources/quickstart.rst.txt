0b70279336c63e6b81ae9e466f1cc7eac3a87292
Gabbyton <gop2@case.edu>
2025-07-21 12:55:10 -0400

    created scaffold for documentation

---
+++ docs/build/html/_sources/quickstart.rst.txt
@@ -0,0 +1,18 @@
+Quickstart
+=====
+Before converting
+-----
+
+Install cemento with pip.
+
+.. code-block:: console
+
+    (.venv) $ pip install cemento
+
+Run the following to see its properly installed:
+
+.. code-block:: console
+
+    (.venv) $ cemento --help
+
+It should show the following:
---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+project = "CEMENTO"
+copyright = "2025, Gabriel Obsequio Ponon"
+author = "Gabriel Obsequio Ponon"
+release = "0.8.4"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_design",
+    "sphinx_iframes",
+    "sphinxcontrib.shtest"
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_css_files = ['custom.css']
