23e1b6edd30a3c8bde7f384ea16be82dff811d85
Gabriel Ponon <gop2@case.edu>
2025-07-22 08:56:17 -0400

    changed file path to input path in drawio_turtle

--- cemento/rdf/drawio_to_turtle.py
+++ cemento/rdf/drawio_to_turtle.py
@@ -5,14 +5,14 @@ from cemento.rdf.graph_to_turtle import convert_graph_to_ttl
 
 
 def convert_drawio_to_ttl(
-    file_path: str | Path,
+    input_path: str | Path,
     output_path: str | Path,
     onto_ref_folder: str | Path,
     defaults_folder: str | Path,
     prefixes_path: str | Path,
     log_substitution_path: str | Path = None,
 ) -> None:
-    graph = read_drawio(file_path, onto_ref_folder, prefixes_path, defaults_folder)
+    graph = read_drawio(input_path, onto_ref_folder, prefixes_path, defaults_folder)
     convert_graph_to_ttl(
         graph,
         output_path,
